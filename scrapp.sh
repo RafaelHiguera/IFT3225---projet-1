97d8033160d5cb2d5856504c9fe4ac9da7041d03
Rafael David Higuera Silva <rafdinho@gmail.com>
2021-02-06 09:26:55 -0500

    Cleaning et simplification du grep script

--- scrapp.sh
+++ scrapp.sh
@@ -5,6 +5,5 @@ nextPage="css3-modsel-1.html"
 for (( i=1; i <= 172; ++i ))
 do
     curl "https://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/html/full/flat/$nextPage" --output "$i.html"
-    nextPage="$(cat "$i.html" | grep -o -E "rel=\"next\" href=[\"'](.*)[\"'] " | sed -n 's/.*href="\([^"]*\).*/\1/p')"
-    echo $nextPage
+    nextPage="$(cat "$i.html" | grep -o -E "rel=\"next\" .*" | sed -n 's/.*href="\([^"]*\).*/\1/p')"
 done
