db5bd275f2933289a68e278507a508c5386be223
Rafael David Higuera Silva <rafdinho@gmail.com>
2021-02-12 10:51:53 -0500

    Randomising strings in the answer

--- extractor.py
+++ extractor.py
@@ -1,6 +1,31 @@
 import sys
-from shutil import copyfile
+import random
+import string
 from bs4 import BeautifulSoup
+
+# This function takes a string and between > < puts random characters
+def scrambleText(text):
+    openingParenthisisFlag = 0;
+    result = "";
+    spaceFlag = False
+    for i in range(0, len(text)):
+        if text[i] == ' ' or text[i] == '\n':
+            spaceFlag = True
+        elif text[i] == '<':
+            openingParenthisisFlag += 1
+        elif text[i] == '>':
+            openingParenthisisFlag -= 1
+            result += '>'
+            continue
+
+        if openingParenthisisFlag == 0 and spaceFlag == False:
+            result += random.choice(string.ascii_letters);
+        else:
+            result += text[i]
+        spaceFlag = False
+    return result
+
+
 testPage = sys.argv[1]
 pageNumber = sys.argv[2]
 templatePage = "template.html"
@@ -18,7 +43,7 @@ with open(templatePage) as file:
     tempSolution = soup.select(".solution")[0]
 
 tempCss.insert(0, css)
-tempHtml.insert(0, html)
-tempSolution.insert(0, htmlText)
+tempHtml.insert(0, scrambleText(html.get_text()))
+tempSolution.insert(0, BeautifulSoup(scrambleText(str(htmlText)), "html.parser"))
 
 print(soup.prettify())
