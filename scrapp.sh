b052ace48e512871472e7dbb216231feeac5194a
Jugklgl <48073914+Jugklgl@users.noreply.github.com>
2021-02-19 23:18:07 -0500

    Probably fixed: Hyperlinks inside html pages working and creating directory if not existing

--- scrapp.sh
+++ scrapp.sh
@@ -1,11 +1,10 @@
+
 #!/bin/bash
-mkdir $1
+mkdir -p $1
 cd $1
 nextPage="css3-modsel-1.html"
-for (( i=1; i <= 172; ++i ))
+for (( i=1; i <= 10; ++i ))
 do
-    curl "https://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/html/full/flat/$nextPage" --output "$i.html"
-    nextPage="$(cat "$i.html" | grep -o -E "rel=\"next\" .*" | sed -n 's/.*href="\([^"]*\).*/\1/p')"
+  curl "https://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/html/full/flat/$nextPage" --output "${nextPage%%.*}.html"
+  nextPage="$(cat "${nextPage%%.*}.html" | grep -o -E "rel=\"next\" .*" | sed -n 's/.*href="\([^"]*\).*/\1/p')"
 done
-
-# Lien entre les pages a regler TODO
